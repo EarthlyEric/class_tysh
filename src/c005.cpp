a695789080e3b952076b2bf91157d366824c3331
EarthlyEric6 <earthlyeric6@gmail.com>
2024-04-05 10:00:42 +0000

    Add code for comparing two strings and counting matching characters

--- src/c005.cpp
+++ src/c005.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
 int main() {
+    // 宣告a和b為0
     int a = 0, b = 0;
+    // 宣告 x 和 y 為長度為4的字元陣列
     char x[4], y[4];
+    // 輸入 x 和 y。
     std::cin >> x >> y;
+    
     for (int i = 0; i < 4; i++) {
+        // 如果 x[i] 等於 y[i] 則 a 加1
         if (x[i] == y[i]) {
             a++;
         } else {
+            // 否則 檢查 x[i] 是否在 y 中
             for (int j = 0; j < 4; j++) {
+                // 如果有，則 b 加1。
                 if (x[i] == y[j]) {
                     b++;
+                    // 並且跳出迴圈
                     break;
                 }
             }
         }
     }
+    // 輸出 a 和 b。
     std::cout << a << "A" << b << "B";
-    return 0;
 }
--- src/c016.cpp
+++ src/c016.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 
 int f91(int n) {
+    //如果 N <= 100, 那麼 f91(N) = f91( f91( N+11) )
+    //如果 N >= 101, 那麼 f91(N) = N-10
+    //規則如上，但實際上只要 N <= 100，f91(N)恆等於 91
+
+    // N>101時，return n-10的值
     if (n >101) {
         return n-10;
     } else {
+        // N<=100時，return 91
         return 91;
     }
 }
 
 int main() {
+    // 宣告一個整數變數x，
     int x;
+    // 當輸入的x不等於0時，持續執行，並輸出f91(x)的值，X=0時結束
     while (std::cin >> x,x != 0) {
+        // 輸出f91(x)的值
         std::cout << "f91(" << x << ") = " << f91(x) << std::endl;
     }
 }
